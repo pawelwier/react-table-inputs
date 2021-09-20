48c5c44a2effa8da4bd599540a8a79e9b2f422a1
Pawel Wier <pawiercinski@gmail.com>
2021-09-20 19:54:51 +0200

    en

--- src/components/MilestonePopup.jsx
+++ src/components/MilestonePopup.jsx
@@ -5,8 +5,8 @@ export default function MilestonePopup({milestone, setMilestoneDescription, subm
         <div>
             <form onSubmit={(e) => submitMilestoneDescription(e, milestone.monthId)}>
                 <input value={milestone ? milestone.description : ''} onChange={(e) => setMilestoneDescription(e.target.value)} />
-                <input type="submit" value="Wyślij" />
-                {milestone.description ? <button onClick={(e) => deleteMilestone(e, milestone)}>Usuń</button> : null}
+                <input type="submit" value="Submit" />
+                {milestone.description ? <button onClick={(e) => deleteMilestone(e, milestone)}>Delete</button> : null}
             </form>
         </div>
     )
